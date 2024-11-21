f029e31527e27107fc7fbea4e718ae964cc55c38
Elena <ele92gris@gmail.com>
2024-11-20 21:05:52 -0500

    fix Controlador cors y app

--- src/main/java/apifestivos/apifestivos/presentacion/FestivoControlador.java
+++ src/main/java/apifestivos/apifestivos/presentacion/FestivoControlador.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ import apifestivos.apifestivos.dominio.DTOs.FestivoDTO;
 /**
  * Controlador REST para manejar las solicitudes relacionadas con los festivos.
  */
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/festivos")
 public class FestivoControlador {
